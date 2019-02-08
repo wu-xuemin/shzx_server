5cf0226cecaf77212bffedb83e0560d4d3623784
Hu Tong <htbasketball@163.com>
2019-02-09 01:35:11 +0800

    1.校车线路的增删改 2.数据库中给admin加上全部页面的访问权限

--- src/main/java/com/eservice/api/dao/BusLineMapper.java
+++ src/main/java/com/eservice/api/dao/BusLineMapper.java
@@ -5,6 +5,7 @@ import com.eservice.api.model.bus_line.BusLine;
 import com.eservice.api.model.bus_line.BusLineInfo;
 import com.eservice.api.model.student.StudentInfo;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -14,4 +15,6 @@ public interface BusLineMapper extends Mapper<BusLine> {
     List<BusLineInfo> getBusLineInfoBySchoolPartition(@Param("schoolPartition")String schoolPartition);
     List<StudentInfo> getStudents(@Param("busNumber")String busNumber,@Param("busMode")String busMode);
     BusLine getBusLineInfoByBusNumberAndBusMode( @Param("busNumber")String busNumber,@Param("busMode")String busMode);
+    @Select("select * from bus_line where valid = 1 and ( name like '%${queryKey}%' or stations like '%${queryKey}%')")
+    List<BusLine> list(@Param("queryKey")String queryKey);
 }
