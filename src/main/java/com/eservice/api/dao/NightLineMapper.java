18ff9ed2d329614a5480b44d2558b6880ed2229f
wu-xuemin <wuxuemin2000@126.com>
2019-02-11 16:00:03 +0800

    1.处理了valid为0的无效数据；2.删除了弃用的NightLine相关文件

--- src/main/java/com/eservice/api/dao/NightLineMapper.java
+++
@@ -1,7 +0,0 @@
-package com.eservice.api.dao;
-
-import com.eservice.api.core.Mapper;
-import com.eservice.api.model.night_line.NightLine;
-
-public interface NightLineMapper extends Mapper<NightLine> {
-}
